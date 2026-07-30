a6b20d05b67afddcbb9289d770a3a10254c15b23
yashasgowda08 <yashasboss555@gmail.com>
2026-07-30 19:23:09 +0530

    Time: 0 ms (100%), Space: 19.2 MB (48.74%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        freq = Counter(word)
+        counts = sorted(freq.values(),reverse=True)
+        pushes = 0
+        for i, f in enumerate(counts):
+            pushes += (i // 8 + 1) * f
+
+        return pushes    
