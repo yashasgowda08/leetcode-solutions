352dbe4e04ef267845018831495fbaeb3300420e
yashasgowda08 <yashasboss555@gmail.com>
2026-08-01 20:35:20 +0530

    Time: 0 ms (100%), Space: 19.9 MB (20.24%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,16 @@
+from functools import lru_cache
+from typing import List
+
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        @lru_cache(None)
+        def dp(i, j):
+            if i == j:
+                return nums[i]
+
+            return max(
+                nums[i] - dp(i + 1, j),
+                nums[j] - dp(i, j - 1)
+            )
+
+        return dp(0, len(nums) - 1) >= 0
